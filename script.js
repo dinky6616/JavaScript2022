e04205bcdd34d04d4f1b4a2512e4256f15bd1c5e
dinky6616 <80316401+dinky6616@users.noreply.github.com>
2022-06-17 10:03:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const country = "Norway";
+//country = 'Sweden' < CONST VARIABLER KAN IKKE ENDRES, DA FÅR VI EN ERROR! :-)
+const continent = 'Europe';
+let population = 5379000;
+let populationFinland = 6000000
+const populationAverage = 33000000
+let description = 'Portugal is in Europe, and its 11 million people speak portuguese';
+// Data Types Assignment
+const isIsland = false;
+const language = 'Norwegian';
+//Console
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(typeof isIsland)
+console.log(typeof country)
+console.log(typeof population)
+console.log(typeof language)
+console.log(
+    'Norway\'s population divided by two is: ' + (population / 2),
+    'Norway\'s population increased by one is: ' + (population + 1),
+    'Is it true or false that Norway\'s population is greater than Finlands? ' + (population >= populationFinland),
+    'Does Norway have less people than the average 33mil per country? ' + (population >= populationFinland)
+);
+
+//Variables Data 1
+const markHeight = 169;
+const johnHeight = 195;
+const markWeight = 78;
+const johnWeight = 92;
+//Variables Data 2
+const markHeight2 = 188;
+const johnHeight2 = 176;
+const markWeight2 = 95;
+const johnWeight2 = 85;
+
+//let bmiCalc = markWeight / markHeight ** 2 = markWeight (markHeight * markHeight)
+function bmiCalc() {
+    if 
+    else if 
+  
+}
+document.getElementById("")
